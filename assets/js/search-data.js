0c697289ff9a073e1c37b9bc3fe56b48a6d2cbb6
Pengxiang-Li <51403237+Pengxiang-Li@users.noreply.github.com>
2025-04-06 16:01:47 +0000

    Deploying to gh-pages from @ Pengxiang-Li/pengxiang-li.github.io@10b32f298e3e31020fb2783a1c7532c2d18bab9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,16 +384,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-2025-01-one-paper-on-multimodal-agent-tuning-is-accepted-by-iclr-2025-spotlight",
+          title: '[2025.01] 🌟 One paper on Multimodal Agent Tuning is accepted by ICLR 2025...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
